4c52a2d270ece7b5e52fed4868cb06f0e7ad4d9a
Lucas Sousa <lucassousa@maisamigas.com.br>
2018-03-11 15:14:07 -0300

    game spec and computer random play

--- computer.rb
+++ computer.rb
@@ -1,4 +1,7 @@
 require './player'
 
 class Computer < Player
+  def play
+    return Random.rand(4)
+  end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,20 +1,30 @@
 require './game'
 
 RSpec.describe Game do
-  context "#check game end" do
-    it "verify if game is over" do
+  context "#check play not winning" do
+    it "play not winning" do
       game = Game.new
       game.addPlay(2, 'x')
       game.addPlay(3, 'y')
       expect(game.isWinningPlay(4,'x')).to be_falsey
     end
   end
-  context "#check game not end" do
-    it "vadasd aooad aiaisdr" do
+  context "#check winning play" do
+    it "play winning" do
       game = Game.new
       game.addPlay(2, 'x')
       game.addPlay(3, 'x')
       expect(game.isWinningPlay(4,'x')).to be_truthy
+    end
+  end
+  context "#check end game tied" do
+    it "end game tied" do
+      game = Game.new
+      game.addPlay(2, 'x')
+      game.addPlay(3, 'x')
+      expect(game.isWinningPlay(4,'x')).to be_falsey
+      expect(game.isFullBoard).to be_truthy
+      expect(game.endGame).to be_truthy
     end
   end
 end
